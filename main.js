5da55da83533015b44079319e89332c67519a6a2
gdani <155196741+gdaniunicorn@users.noreply.github.com>
2025-05-25 14:20:48 +0200

    js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const ages = ["1-20","21-80","100-1000","1000+"]
-
 //set the max height of the cards div
 var cards = document.getElementById("cards");
 cards.style.maxHeight = window.innerHeight - cards.getBoundingClientRect().top - 10 + "px";
@@ -12,7 +10,7 @@ function loadFile(file,func){
         var lines = text.trim().split("\n");
         var headers = lines[0].split(',');
 
-        var data = [];
+        var d = [];
 
         for(let i=1;i<lines.length;i++){
             var dict = {};
@@ -22,117 +20,144 @@ function loadFile(file,func){
                 dict[headers[j].trim()] = datas[j].trim();
             }
 
-            data.push(dict);
+            d.push(dict);
         }
         //console.log(data);
         
-        func(data);
+        func(d);
       })
       .catch((e) => console.error(e));
 }
 
+
+
 //start the game
-document.onload = loadFile("characters.txt",main)
-//retry
-document.getElementById("retry").addEventListener("click",function(){
-  document.getElementById("cards").innerHTML = "";
-  document.getElementById("guess").value = "";
-  loadFile("characters.txt",main);
-})
+  document.onload = loadFile("characters.txt",main)
 
 
-/** MAIN FUNCTION
- *  @param {Array} data - Array of characters
+/**
+ *  -- MAIN FUNCTION -- 
+ * @param {Array} file 
  */
-function main(data){
+function main(file){
 
-  //variables
-  var input = document.getElementById("guess");
-  var guesschar = undefined;
-  var sol=undefined;
-  var j = document.getElementById("j"); //javaslatok
+    var data = undefined;
 
-  //generate the solution character
-  var rand = Math.floor(Math.random()*(data.length-1))
-  while (rand == localStorage.getItem("last")){
-    var rand = Math.floor(Math.random()*(data.length-1))
-  }
-  sol = data[rand]
-  console.log(sol);
-  localStorage.setItem("last", rand);
+  // retry \\
+    document.getElementById("retry").addEventListener("click",function(){
+        document.getElementById("cards").innerHTML = "";
+        document.getElementById("guess").value = "";
+        game();
+    })
 
-  //remove previous eventlisteners
-  input.removeEventListener("input",oninput);
-  document.getElementById("send").removeEventListener("click",send);
-  document.removeEventListener("keyup",onkeyup);
+  // VARIABLES \\
+    var input = document.getElementById("guess");
+    var guesschar = undefined;
+    var sol=undefined;
+    var j = document.getElementById("j"); //javaslatok
 
-  // main input function
-  function oninput(){
-    if(input.value!="" && input.value!=input.value.length*" "){
-      var chars = data.filter(c => c.sname.toLowerCase().includes(input.value.toLowerCase()));  //available characters
-      console.log(chars);
-      j.innerHTML = chars.map(c => 
-        `<div class="card-j" id="${c.code}">
-            <div class="j-img" style="background-image:url(pics/${c.code}.webp);"></div>
-            <div class="j-name">${c.name}</div>
-          </div>`).join("");
-      // |^ javaslatok hozzáadása
-      j.style.display = "flex";
 
-      //eventlisteners minden javaslatra
-      Array.from(document.getElementsByClassName("card-j")).forEach(e => {
-        e.addEventListener("click",function(){guesschar=e.id;guess();})
-      })
+  // define eventlistener handlers \\
+    function send(){
+        if(input.value.trim()!=="" && j.children.length>0){
+            guesschar=j.children[0].id;
+            guess();
+        }
     }
-    else{
-      //javaslatok eltüntetése
-      j.innerHTML = "";
-      j.style.display = "none";
+    function onkeyup(e){
+        if(e.key=="Enter" && input.value.trim()!=="" && j.children.length>0){
+            guesschar = j.children[0].id;
+            guess();
+        }
     }
-  }
-  
-  
-  //add eventlisteners
-  input.addEventListener("input",oninput);
+    function onclick(e){
+        let card = e.target.closest(".card-j");
+            if (card){
+            guesschar = card.id;
+            guess();
+        }
+    }
 
-  document.getElementById("send").addEventListener("click",send)
-  function send(){
-    if(input.value.trim()!=="" && j.children.length>0){
-      guesschar=j.children[0].id;
-      guess();
-    }
-  }
-  document.addEventListener("keyup",onkeyup)
-  function onkeyup(e){
-    if(e.key=="Enter" && input.value.trim()!=="" && j.children.length>0){
-      guesschar = j.children[0].id;
-      guess();
-    }
-  }
+  // EVENT LISTENERS \\
+    input.addEventListener("input",oninput);
+    document.getElementById("send").addEventListener("click",send)
+    document.addEventListener("keyup",onkeyup)
+    j.addEventListener("click",onclick)
 
 
-  // guess function
-  function guess(){
-    var out = document.getElementById("cards")
-    var char = data.find(c => c.code === guesschar)
-    out.innerHTML = `
-      <div class="card">
-        <div class="card-attr card-img" style="background-image:url(pics/${char.code}.webp);"></div>
-        <div class="card-name card-attr ${char.name==sol.name ? 'correct':'incorrect'}">${char.name}</div>
-        <div class="card-attr card-gender ${char.gender==sol.gender ? 'correct':'incorrect'}">${char.gender}</div>
-        <div class="card-attr card-race ${char.race==sol.race ? 'correct':'incorrect'}">${char.race}</div>
-        <div class="card-attr card-age ${char.age==sol.age ? 'correct':'incorrect'}">${char.age} <div class="index up"></div> </div>
-        <div class="card-attr card-height ${char.height==sol.height ? 'correct':'incorrect'}">${char.height} <div class="index down"></div></div>
-        <div class="card-attr card-hair ${char.hair==sol.hair ? 'correct':'incorrect'}">${char.hair}</div>
-        <div class="card-attr card-location ${char.location==sol.location ? 'correct':'incorrect'}">${char.location}</div>
-        <div class="card-attr card-arc ${char.arc==sol.arc ? 'correct':'incorrect'}">${char.arc}</div>
-      </div>` + out.innerHTML
-    // |^ add char datas
+
+
+
+  //  --INPUT FUNCTION--  \\
+    function oninput(){
+        if(input.value.trim()!==""){
+            console.log(data);
+            
+            var chars = data.filter(c => c.sname.toLowerCase().includes(input.value.toLowerCase()));  //available characters
+            j.innerHTML = chars.map(c => 
+            `<div class="card-j" id="${c.code}">
+                <div class="j-img" style="background-image:url(pics/${c.code}.webp);"></div>
+                <div class="j-name">${c.name}</div>
+                </div>`).join("");
+            // |^ javaslatok hozzáadása
+            j.style.display = "flex";
+        }else{
+            //javaslatok eltüntetése
+            j.innerHTML = "";
+            j.style.display = "none";
+        }
+    }
+
+  //  --GUESS FUNCTION--  \\
+    function guess(){
+        var out = document.getElementById("cards")
+        var char = data.find(c => c.code === guesschar)
+        if(!char){console.error("Character not found:", guesschar);return;}
+        out.innerHTML = `
+        <div class="card">
+            <div class="card-attr card-img" style="background-image:url(pics/${char.code}.webp);"></div>
+            <div class="card-name card-attr ${char.name==sol.name ? 'correct':'incorrect'}">${char.name}</div>
+            <div class="card-attr card-gender ${char.gender==sol.gender ? 'correct':'incorrect'}">${char.gender}</div>
+            <div class="card-attr card-race ${char.race==sol.race ? 'correct':'incorrect'}">${char.race}</div>
+            <div class="card-attr card-age ${char.age==sol.age ? 'correct':'incorrect'}">${char.age} <div class="index up"></div> </div>
+            <div class="card-attr card-height ${char.height==sol.height ? 'correct':'incorrect'}">${char.height} <div class="index down"></div></div>
+            <div class="card-attr card-hair ${char.hair==sol.hair ? 'correct':'incorrect'}">${char.hair}</div>
+            <div class="card-attr card-location ${char.location==sol.location ? 'correct':'incorrect'}">${char.location}</div>
+            <div class="card-attr card-arc ${char.arc==sol.arc ? 'correct':'incorrect'}">${char.arc}</div>
+        </div>` + out.innerHTML
+    //|^ add char datas
     // remove guessed char
-    data.splice(data.findIndex(c => c.code === guesschar),1);
-    input.value="";
-    j.style.display = "none";
-  }
+        if(char.code !== sol.code){
+            data.splice(data.findIndex(c => c.code === guesschar),1);
+            input.value="";
+            j.style.display = "none";
+        }else{
+            window.alert("Kitaláltad!!!");
+        }
+    }
+
+  // WIN FUNCTION \\
+    function win(){
+    }  
+
+
+
+  // -- MAIN GAME FUNCTION --
+    function game(){
+      // setup
+        data = file.slice();
+        console.log(data,file);
+        
+      // generate the solution character
+        var rand = Math.floor(Math.random()*(data.length-1))
+        while (rand == localStorage.getItem("last")){
+            rand = Math.floor(Math.random()*(data.length-1))
+        }
+        sol = data[rand]
+        console.log(sol);
+        localStorage.setItem("last", rand);
+    }
+
+  // START THE GAME \\
+    game();
 }
-
-
